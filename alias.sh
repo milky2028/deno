5da3ed7c8791c1ff38b2c0b40c9e5765a95642a0
Tyler <tylergross28@gmail.com>
2021-12-23 09:34:11 -0500

    spacing

--- alias.sh
+++ alias.sh
@@ -2,5 +2,5 @@
 
 alias serve="deno run --allow-net --allow-read src/server.ts"
 alias compile="rm -rf build && \
-                   mkdir build && \
-                   deno compile --output ./build/server --allow-net --allow-read ./src/server.ts"
+               mkdir build && \
+               deno compile --output ./build/server --allow-net --allow-read ./src/server.ts"
