01918bbdb7c4ffc1ca7a29892652dfa33cd37c97
Tyler <tylergross28@gmail.com>
2021-12-23 09:44:34 -0500

    add prod serve command

--- alias.sh
+++ alias.sh
@@ -4,3 +4,4 @@ alias serve="deno run --allow-net --allow-read src/server.ts"
 alias compile="rm -rf build && \
                mkdir build && \
                deno compile --output ./build/server --allow-net --allow-read ./src/server.ts"
+alias serve:prod="compile && ./build/server"
