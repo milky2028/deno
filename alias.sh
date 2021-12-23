fa43c2909975deef15d01af4f75fd55156851a87
Tyler <tylergross28@gmail.com>
2021-12-23 12:44:42 -0500

    more fixes for alias commands

--- alias.sh
+++ alias.sh
@@ -1,13 +1,14 @@
 #!/bin/zsh
 
 alias serve="DENO_DIR=.cache deno run --allow-net --allow-read src/server.ts"
-alias compile="echo 'Setting up build directory...' && \
-               rm -rf build && \
-               mkdir build && \
-               echo 'Checking formatting...' && \
-               deno fmt --check && \
-               echo 'Linting...' && \
-               deno lint && \
-               echo 'Compiling...' && \
-               DENO_DIR=.cache deno compile --output ./build/server --allow-net --allow-read ./src/server.ts"
-alias serve:prod="compile && ./build/server"
+alias compile="DENO_DIR=.cache deno compile --output ./build/server --allow-net --allow-read ./src/server.ts"
+alias build:prod="echo 'Setting up build directory...' && \
+                  rm -rf build && \
+                  mkdir build && \
+                  echo 'Checking formatting...' && \
+                  deno fmt --ignore=./.cache --check && \
+                  echo 'Linting...' && \
+                  deno lint --ignore=./.cache && \
+                  echo 'Compiling...' && \
+                  compile"
+alias serve:prod="build:prod && ./build/server"
