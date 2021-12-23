218627dee6f07a95498aadb85219e1218ce7157a
Tyler <tylergross28@gmail.com>
2021-12-23 09:38:42 -0500

    only allow read

--- src/handlers/serveHtml.ts
+++ src/handlers/serveHtml.ts
@@ -3,6 +3,9 @@ import { readableStreamFromReader } from "../deps.ts";
 const relative = (path: string) => new URL(path, import.meta.url);
 
 export default async function serverHtml() {
-  const file = await Deno.open(relative("../static/index.html"));
+  const file = await Deno.open(relative("../static/index.html"), {
+    read: true,
+  });
+
   return new Response(readableStreamFromReader(file));
 }
