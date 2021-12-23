56600021ed15c41fe0797d5006eaa6107e8b1b00
Tyler <tylergross28@gmail.com>
2021-12-23 10:16:44 -0500

    better alias commands

--- alias.sh
+++ alias.sh
@@ -1,7 +1,13 @@
 #!/bin/zsh
 
 alias serve="deno run --allow-net --allow-read src/server.ts"
-alias compile="rm -rf build && \
+alias compile="echo 'Setting up build directory...' && \
+               rm -rf build && \
                mkdir build && \
+               echo 'Checking formatting...' && \
+               deno fmt --check && \
+               echo 'Linting...' && \
+               deno lint && \
+               echo 'Compiling...' && \
                deno compile --output ./build/server --allow-net --allow-read ./src/server.ts"
 alias serve:prod="compile && ./build/server"
