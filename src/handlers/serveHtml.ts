740e9429ed5be6739da33f68c2c4712ad0b94dce
Tyler <tylergross28@gmail.com>
2022-01-05 15:30:31 -0500

    typo

--- src/handlers/serveHtml.ts
+++ src/handlers/serveHtml.ts
@@ -2,7 +2,7 @@ import { readableStreamFromReader } from "../deps.ts";
 
 const relative = (path: string) => new URL(path, import.meta.url);
 
-export default async function serverHtml() {
+export default async function serveHtml() {
   const file = await Deno.open(relative("../static/index.html"), {
     read: true,
   });
